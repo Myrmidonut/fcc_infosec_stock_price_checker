af9f878f78eb1efb10f3992cde507d5bfce4d08f
Glitch (fcc-infosec-stock-price-checker-fred) <none>
2018-11-05 00:40:58 +0000

    code style rework, removed inline styles/scripts, added form route

--- server.js
+++ server.js
@@ -1,35 +1,31 @@
 'use strict';
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+const express           = require('express');
+const bodyParser        = require('body-parser');
+const expect            = require('chai').expect;
+const cors              = require('cors');
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
+const nodeFetch         = require("node-fetch");
+const helmet            = require("helmet");
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+const app = express();
 
-const nodeFetch = require("node-fetch");
-
-var app = express();
-
-// --- helmet
-const helmet = require("helmet");
-app.use(helmet.contentSecurityPolicy({directives: { defaultSrc: ["'self'"], styleSrc: ["'self'"] } }));
-// ---
-
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    styleSrc: ["'self'"]
+  }
+}));
 app.use('/public', express.static(process.cwd() + '/public'));
-
 app.use(cors({origin: '*'})); //For FCC testing purposes only
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 //Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+  .get((req, res) => res.sendFile(process.cwd() + '/views/index.html'));
 
 //For FCC testing purposes
 fccTestingRoutes(app);
@@ -38,24 +34,26 @@ fccTestingRoutes(app);
 apiRoutes(app);  
     
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.status(404)
     .type('text')
     .send('Not Found');
 });
 
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
+  
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
-    setTimeout(function () {
+    
+    setTimeout(() => {
       try {
         runner.run();
-      } catch(e) {
-        var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
+      }
+      catch(error) {
+        console.log('Tests are not valid:');
+        console.log(error);
       }
     }, 3500);
   }
